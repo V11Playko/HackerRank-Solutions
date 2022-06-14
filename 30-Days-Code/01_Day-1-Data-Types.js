a59455ab4d8370207ebec5bb5d3816db4a295876
V11Playko <heinnervega20@gmail.com>
2022-06-14 14:53:05 -0500

    Day 1 <Done>

---
+++ 30-Days-Code/01_Day-1-Data-Types.js
@@ -0,0 +1,13 @@
+// readLine: The Readline module provides a way to read a stream of data, one line at a time.
+
+let i2 = +(readLine());
+let d2 = +(readLine());
+let s2 = readLine();
+
+console.log(i + i2);
+
+//.toFixed: The toFixed() method formats a number using fixed point notation.
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+console.log((d + d2).toFixed(1));
+
+console.log(s +s2)
