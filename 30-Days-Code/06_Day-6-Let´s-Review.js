81a025ec6c8f87a9a1a6caef4f70306f952e02af
V11Playko <heinnervega20@gmail.com>
2022-06-14 15:13:24 -0500

    Day 6 <Done>

---
+++ 30-Days-Code/06_Day-6-Let´s-Review.js
@@ -0,0 +1,36 @@
+function processData(input) {
+    // Split: The split() method splits an object of type String into an array (vector) of strings by separating the string into substrings.
+    const inputs = input.split('\n');
+
+    if (!inputs || inputs.length < 2) {
+        return;
+    }
+
+    const [testCases, ...stringsToCheck] = inputs;
+
+    if (testCases < 1 || testCases > 10) {
+        return;
+    }
+
+    for (const stringToCheck of stringsToCheck) {
+        if (!stringToCheck || stringToCheck.length < 2 || stringToCheck.length > 10000) {
+            continue;
+        }
+
+        const isEven = num => num % 2 ? false : true;
+        let evenGroup = '';
+        let oddGroup = '';
+
+        stringToCheck
+            .split('')
+            .forEach((letter, index) => {
+                if (isEven(index)) {
+                    evenGroup += letter;
+                } else {
+                    oddGroup += letter;
+                }
+            });
+
+        console.log(`${evenGroup} ${oddGroup}`);
+    }
+}
