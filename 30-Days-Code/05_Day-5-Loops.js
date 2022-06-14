91b3c8ba6cd6f1744a470ff4b1951c06d789e46e
V11Playko <heinnervega20@gmail.com>
2022-06-14 15:12:27 -0500

    Day 5 <Update>

--- 30-Days-Code/05_Day-5-Loops.js
+++ 30-Days-Code/05_Day-5-Loops.js
@@ -1,5 +1,5 @@
 function main() {
-    // The trim() method removes whitespace at both ends of the string.
+    // trim(): The trim() method removes whitespace at both ends of the string.
     const n = parseInt(readLine().trim(), 10);
     for(let i=1;i <= 10; i++){
         var result = n * i;
