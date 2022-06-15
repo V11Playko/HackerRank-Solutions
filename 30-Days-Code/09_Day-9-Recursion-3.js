b3ab581d5652dda4298c9fba8144a25534623833
V11Playko <heinnervega20@gmail.com>
2022-06-15 06:57:30 -0500

    Day 9 <Done>

---
+++ 30-Days-Code/09_Day-9-Recursion-3.js
@@ -0,0 +1,16 @@
+
+function factorial(n) {
+    if(n <= 1){
+        return 1;
+    } else{
+        return n*factorial(n-1);
+    }
+}
+
+// how this works
+
+// factorial(5)
+// 5*5-1 = 20
+// 20*4-1 = 60
+// 60*3-1 = 120
+// 120*2-1(returns 1) = 120
