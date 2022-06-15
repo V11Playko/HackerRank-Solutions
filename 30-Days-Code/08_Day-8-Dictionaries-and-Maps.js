719cbc42aa1e9f61ff3ab1fa9622def9fd914a74
V11Playko <heinnervega20@gmail.com>
2022-06-15 06:52:42 -0500

    Day 8 <Done>

---
+++ 30-Days-Code/08_Day-8-Dictionaries-and-Maps.js
@@ -0,0 +1,20 @@
+    //Enter your code here
+    input = input.split("\n");
+
+    let inputs = parseInt(input[0]);
+
+    let map = new Map();
+    for (let i = 1; i <= inputs; i++) {
+        map.set(input[i].split(" ")[0], input[i].split(" ")[1])
+    }
+
+    // console.log(map)
+
+    for (let j = inputs + 1; j < input.length; j++) {
+        // Get: Binds the property of an object to a function that will be called when the property is fetched.
+        if (map.get(input[j]) != undefined) {
+            console.log(input[j] + "=" + map.get(input[j]));
+        } else {
+            console.log("Not found");
+        }
+    }
