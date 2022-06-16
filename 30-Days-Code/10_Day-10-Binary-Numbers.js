a7ce73b138d4042bd40c78be23f9cc0624fbfdfe
V11Playko <heinnervega20@gmail.com>
2022-06-16 06:19:28 -0500

    Day 10 <Update>

--- 30-Days-Code/10_Day-10-Binary-Numbers.js
+++ 30-Days-Code/10_Day-10-Binary-Numbers.js
@@ -1,7 +1,9 @@
+// toString: Returns a string representing the object.
 var n = parseInt(readLine()).toString(2);
     var splits = n.split('0');
     console.log(splits.map(function(elem)
     {return elem.length;})
+    // reduce: The reduce() method executes a reducer function on each element of an array, returning a single value as a result.
     .reduce(function(a,b){
         if (a>b) return a; else return b;}));
 
