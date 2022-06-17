21223b45945eddbdd6cbd5f04eb24e1571aa5889
V11Playko <heinnervega20@gmail.com>
2022-06-17 06:58:36 -0500

    Day 15 <Update-2>

--- 30-Days-Code/15_Day-15-Linked-List.js
+++ 30-Days-Code/15_Day-15-Linked-List.js
@@ -1,4 +1,4 @@
-// the value of this is determined by how the function is called. It cannot be set by an assignment at run time, and can be different each time the function is called.
+// this: the value of this is determined by how the function is called. It cannot be set by an assignment at run time, and can be different each time the function is called.
 
 // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/this
 
@@ -6,6 +6,8 @@ this.insert=function(head,data){
     //complete this method
     var newNode = new Node(data);
 
+    // typeOf: The typeof operator returns a string indicating the type of the operand without evaluating it.
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/typeof
     if (head === null || typeof head === 'undefined') {
         head = newNode;  
     } else if (head.next === null) {
