c4e86e63bb84e28e00fb30ee104ba785628b6394
V11Playko <heinnervega20@gmail.com>
2022-06-17 06:56:54 -0500

    Day 15 <Update>

--- 30-Days-Code/15_Day-15-Linked-List.js
+++ 30-Days-Code/15_Day-15-Linked-List.js
@@ -1,4 +1,6 @@
+// the value of this is determined by how the function is called. It cannot be set by an assignment at run time, and can be different each time the function is called.
 
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/this
 
 this.insert=function(head,data){
     //complete this method
