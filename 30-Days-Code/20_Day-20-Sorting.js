280a3e13cceb7980cd8557ef2870160139e8316c
V11Playko <heinnervega20@gmail.com>
2022-06-18 20:02:28 -0500

    Day 15 <Update>

--- 30-Days-Code/20_Day-20-Sorting.js
+++ 30-Days-Code/20_Day-20-Sorting.js
@@ -1,6 +1,8 @@
 function main() {
     var n = parseInt(readLine());
+    // split: The split() method splits an object of type String into an array (vector) of strings by separating the string into substrings.
     var a = readLine().split(' ');
+    // map: The map() method creates a new array with the results of the call to the indicated function applied to each of its elements.
     a = a.map(Number);
     var totalNum = 0
     for (var i = 0; i < a.length; i++) {
