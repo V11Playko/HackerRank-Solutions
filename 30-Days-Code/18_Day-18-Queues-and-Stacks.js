0d15ea08693e332760e4489d3637006fe7a4e277
V11Playko <heinnervega20@gmail.com>
2022-06-18 20:07:05 -0500

    Day 18 <Update-2>

--- 30-Days-Code/18_Day-18-Queues-and-Stacks.js
+++ 30-Days-Code/18_Day-18-Queues-and-Stacks.js
@@ -5,6 +5,7 @@ function Solution(){
     let queue = [];
     // this: the value of this is determined by how the function is called. It cannot be set by an assignment at runtime, and can be different each time the function is called.
     this.pushCharacter = (n) => {
+        // push: The push() method adds one or more elements to the end of an array and returns the new length of the array.
         stack.push(n);
     }
     this.popCharacter = () => {
