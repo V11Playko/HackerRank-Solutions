dd52ef3b44468a5ab62bc0523662fe4ba5e1d43f
V11Playko <heinnervega20@gmail.com>
2022-06-18 20:05:31 -0500

    Day 18 <Done>

---
+++ 30-Days-Code/18_Day-18-Queues-and-Stacks.js
@@ -0,0 +1,21 @@
+function Solution(){
+    //Write your code here
+    
+    let stack = [];
+    let queue = [];
+    
+    this.pushCharacter = (n) => {
+        stack.push(n);
+    }
+    this.popCharacter = () => {
+        return stack.pop();
+    }
+    
+    this.enqueueCharacter = (n) => {
+        queue.push(n)
+    }
+    this.dequeueCharacter = () => {
+        return queue.shift();
+    }
+  
+  }
