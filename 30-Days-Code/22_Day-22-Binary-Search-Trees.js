187120a4dfc345955a17a3153bc263bed1f306a5
V11Playko <heinnervega20@gmail.com>
2022-06-20 15:14:06 -0500

    Day 22 <Update-3>

--- 30-Days-Code/22_Day-22-Binary-Search-Trees.js
+++ 30-Days-Code/22_Day-22-Binary-Search-Trees.js
@@ -7,5 +7,6 @@
             // For example, the following code returns 120, which is equal to the original height plus the vertical padding. 
             let l = this.getHeight(root.left);
             let r = this.getHeight(root.right);
+            // return: The return statement ends the execution of the function and specifies a value to be returned to the caller of the function.
             return (l>r?l:r)+1;
          }
