4f974ebf25b138d3e36de65a4aff1dff0579b146
V11Playko <heinnervega20@gmail.com>
2022-06-20 15:11:08 -0500

    Day 22 <Update>

--- 30-Days-Code/22_Day-22-Binary-Search-Trees.js
+++ 30-Days-Code/22_Day-22-Binary-Search-Trees.js
@@ -2,6 +2,7 @@
         if(!root){
             return -1;
          } else {
+            // getHeight: In JavaScript, you can use the clientHeight property, which returns an element's height, including its vertical padding. Basically, it returns the actual space used by the displayed content.
             let l = this.getHeight(root.left);
             let r = this.getHeight(root.right);
             return (l>r?l:r)+1;
