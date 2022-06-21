493141db58b594f6572a6e973841f685e9d09165
V11Playko <heinnervega20@gmail.com>
2022-06-21 11:36:06 -0500

    Day 24 <Update-2>

--- 30-Days-Code/24_Day-24-More-Linked-Lists.js
+++ 30-Days-Code/24_Day-24-More-Linked-Lists.js
@@ -5,6 +5,7 @@ this.removeDuplicates=function(head){
     // while: Creates a loop that executes a specified statement as long as a certain condition evaluates to true. This condition is evaluated before executing the statement.
     while(current != null) {
         if(arr[arr.length - 1] != current.data) {
+            // push: The push() method adds one or more elements to the end of an array and returns the new length of the array.
             arr.push(current.data);
         } else {
             prev.next = current.next;
