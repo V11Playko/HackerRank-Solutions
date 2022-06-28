189ccdfd1bce4ec00562e990d1ff6d330ceb934f
V11Playko <heinnervega20@gmail.com>
2022-06-28 07:54:24 -0500

    Day 29 <Done>

---
+++ 30-Days-Code/29_Day-29-Bitwise-AND.js
@@ -0,0 +1,24 @@
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var k = parseInt(n_temp[1]);
+        function findMaxPoss(arr) {
+            var res = 0;
+            for(var i = 0; i < arr.length; i++){
+                for(var j = i + 1; j < arr.length; j++){
+                    var ans = arr[i] & arr[j];
+                    if((ans > res) && (ans < k)){
+                        res = ans;
+                    }
+                }
+            }
+            return res;
+        }
+        console.log(findMaxPoss(range(n)));
+    }
+    function range(n){
+        return Array.apply(null, Array(n)).map(function (_, i) {return i + 1;});
+    }
+}
